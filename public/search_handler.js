523c26a0e370ba01f7f2d5979dfbd630e1db5213
frenki <frenkiqose@gmail.com>
2025-02-06 22:42:04 +0100

    added song banners

--- public/search_handler.js
+++ public/search_handler.js
@@ -34,22 +34,50 @@ async function getData(sentence) {
 }
 
 function display_song(json){
-  console.log(json);
-  
   let main = document.querySelector(".main");
   main.innerHTML = '';
+
   json.forEach(element => {
-    create_song(element.name, main);
+    create_song(element.name, element.artists[0].name, element.album.images[1].url, element.external_urls.spotify, main);
   });
 }
 
-function create_song(title, main){
-  //TO DO : create banner properly and clickable
+function create_song(title, artist, image_url, link, main){
   
-  let p = document.createElement('p');
-  
-  p.innerHTML = title;
-  main.appendChild(p);
+  let banner = document.createElement('div');
+  banner.className = 'banner flex_center_row';
+  banner.onclick = ()=>{
+    location.href = link;
+  }
+
+  let image = document.createElement('div');
+  image.className = 'image';
+  let image_link = document.createElement('img');
+  image_link.src = image_url;
+  image.appendChild(image_link);
+
+  let description = document.createElement('div');
+  description.className = 'description flex_center_col';
+
+  let song_div = document.createElement('div');
+  song_div.className = 'name';
+  let song_name = document.createElement('h2');
+  song_name.innerHTML = title;
+  song_div.appendChild(song_name);
+
+  let artist_div = document.createElement('div');
+  artist_div.className = 'artist';
+  let artist_name = document.createElement('h3');
+  artist_name.innerHTML = artist;
+  artist_div.appendChild(artist_name);
+
+  description.appendChild(song_div);
+  description.appendChild(artist_div);
+
+  banner.appendChild(image);
+  banner.appendChild(description);
+
+  main.appendChild(banner);
 }
 
 function display_error(error){
